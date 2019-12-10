7fc9d6834550613c906aa7fb899dbceba19a5744
Laurens Valk <laurens@pybricks.com>
2019-12-10 15:24:20 +0100

    snippets/ev3/i2c_extra: add snippet

---
+++ snippets/ev3/i2c_extra/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.customdevices import I2CDevice
+
+# Initialize the EV3
+ev3 = EV3Brick()
+
+# Initialize I2C Sensor
+device = I2CDevice(ev3.Port.S2, 0xD2 >> 1)
+
+# Recommended for reading
+result, = device.read(reg=0x0F, length=1)
+
+# Read 1 byte from no particular register:
+device.read(reg=None, length=1)
+
+# Read 0 bytes from no particular register:
+device.read(reg=None, length=0)
+
+# Recommended for writing:
+device.write(reg=0x22, data=b'\x08')
+
+# Write 1 byte to no particular register:
+device.write(reg=None, data=b'\x08')
+
+# Write 0 bytes to no particular register:
+device.write(reg=None, data=b'')
