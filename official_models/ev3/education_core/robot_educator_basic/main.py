15380261c9a83da862a00ec7a6739f4835b9eff2
Laurens Valk <laurens@pybricks.com>
2020-04-24 14:09:12 +0200

    official_models/ev3/educator/basic: flake8

--- official_models/ev3/education_core/robot_educator_basic/main.py
+++ official_models/ev3/education_core/robot_educator_basic/main.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor, ColorSensor
+from pybricks.ev3devices import Motor
 from pybricks.parameters import Port
-from pybricks.tools import wait
 from pybricks.robotics import DriveBase
 
 # Initialize the EV3 Brick.
