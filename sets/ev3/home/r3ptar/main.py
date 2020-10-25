ea240e207da36ad09b6c1ace367ad4b5a997c1ee
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-10-25 13:27:52 -0700

    refactor R3ptar with main.py and README

---
+++ sets/ev3/home/r3ptar/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env pybricks-micropython
+ 
+
+from pybricks.tools import wait
+
+from r3ptar import R3ptar
+
+
+r3ptar = R3ptar()
+
+while True:
+    r3ptar.drive_by_ir_beacon(speed=1000)
+    r3ptar.strike_by_ir_beacon(speed=1000)
+    r3ptar.hiss_if_touched()
+    wait(1)
