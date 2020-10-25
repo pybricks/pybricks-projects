fc5ff77ed5702acddd9390525fef2d56febf3424
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-10-25 13:51:57 -0700

    sets/ev3/home/gripp3r: refactor main.py

--- sets/ev3/home/gripp3r/gripp3r.py
+++ sets/ev3/home/gripp3r/gripp3r.py
@@ -1,21 +1,7 @@
-#!/usr/bin/env pybricks-micropython
-
-"""
-Example LEGO® MINDSTORMS® EV3 Gripp3r Program
----------------------------------------------
-
-This program requires LEGO® EV3 MicroPython v2.0 downloadable at:
-https://education.lego.com/en-us/support/mindstorms-ev3/python-for-ev3
-
-Building instructions can be found at:
-https://www.lego.com/en-us/themes/mindstorms/buildarobot
-"""
-
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, TouchSensor, InfraredSensor
 from pybricks.media.ev3dev import SoundFile
 from pybricks.parameters import Button, Direction, Port, Stop
-from pybricks.tools import wait
 
 from rc_tank_util import RemoteControlledTank
 
@@ -75,26 +61,3 @@ class Gripp3r(RemoteControlledTank):
             while Button.BEACON in \
                     self.ir_sensor.buttons(channel=self.ir_beacon_channel):
                 pass
-
-    def main(
-            self,
-            driving_speed: float = 1000   # mm/s
-            ):
-        """
-        Gripp3r's main program performing various capabilities
-        """
-        self.gripping_motor.run_time(
-            speed=-500,
-            time=1000,
-            then=Stop.COAST,
-            wait=True)
-
-        while True:
-            self.drive_by_ir_beacon(speed=driving_speed)
-            self.grip_or_release_by_ir_beacon(speed=500)
-            wait(1)
-
-
-if __name__ == '__main__':
-    GRIPP3R = Gripp3r()
-    GRIPP3R.main(driving_speed=1000)
