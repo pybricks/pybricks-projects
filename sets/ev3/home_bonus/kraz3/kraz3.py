8355e431f07a0c9f1abe2a633207be33074b2a48
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-12-12 23:37:25 -0800

    refactor Kraz3 program

--- sets/ev3/home_bonus/kraz3/kraz3.py
+++ sets/ev3/home_bonus/kraz3/kraz3.py
@@ -36,112 +36,108 @@ class Kraz3(RemoteControlledTank):
 
         self.color_sensor = ColorSensor(port=color_sensor_port)
 
-    def kungfu_manoeuvre_if_touched_or_remote_controlled(self):
+    def kungfu_manoeuvre_whenever_touched_or_remote_controlled(self):
         """
         Kung-Fu manoeuvre via Touch Sensor and Remote Control of head and arms
         """
-        if self.touch_sensor.pressed():
-            self.ev3_brick.speaker.play_file(file=SoundFile.KUNG_FU)
-
-            self.wiggle_motor.run_angle(
-                speed=500,
-                rotation_angle=360,
-                then=Stop.HOLD,
-                wait=True)
-
-        elif Button.BEACON in \
-                self.ir_sensor.buttons(channel=self.ir_beacon_channel):
-            self.wiggle_motor.run(speed=111)
-
-        else:
-            self.wiggle_motor.stop()
-
-    def kungfu_manoeuvre_whenever_touched_or_remote_controlled(self):
         while True:
-            self.kungfu_manoeuvre_if_touched_or_remote_controlled()
-            wait(10)
+            if self.touch_sensor.pressed():
+                self.ev3_brick.speaker.play_file(file=SoundFile.KUNG_FU)
 
-    def react_to_color(self):
-        detected_color = self.color_sensor.color()
+                self.wiggle_motor.run_angle(
+                    speed=500,
+                    rotation_angle=360,
+                    then=Stop.HOLD,
+                    wait=True)
 
-        if detected_color == Color.YELLOW:
-            self.ev3_brick.speaker.play_file(file=SoundFile.YELLOW)
+            elif Button.BEACON in \
+                    self.ir_sensor.buttons(channel=self.ir_beacon_channel):
+                self.wiggle_motor.run(speed=111)
 
-            self.wiggle_motor.run_angle(
-                speed=-860,
-                rotation_angle=360,
-                then=Stop.HOLD,
-                wait=True)
+            else:
+                self.wiggle_motor.stop()
 
-            self.ev3_brick.speaker.play_file(file=SoundFile.UH_OH)
-
-            wait(500)
-
-            self.ev3_brick.speaker.play_file(file=SoundFile.SNEEZING)
-
-            wait(500)
-
-        elif detected_color == Color.RED:
-            self.ev3_brick.speaker.play_file(file=SoundFile.SHOUTING)
-
-            for _ in range(randint(1, 6)):
-                self.ev3_brick.speaker.play_file(file=SoundFile.SMACK)
-
-            self.ev3_brick.light.on(color=Color.RED)
-
-            self.wiggle_motor.run_angle(
-                speed=170,
-                rotation_angle=360,
-                then=Stop.HOLD,
-                wait=True)
-
-            self.ev3_brick.speaker.play_file(file=SoundFile.LEGO)
-            self.ev3_brick.speaker.play_file(file=SoundFile.MINDSTORMS)
-
-            self.ev3_brick.light.off()
-
-        elif detected_color == Color.BROWN:
-            self.ev3_brick.speaker.play_file(file=SoundFile.BROWN)
-
-            wait(1000)
-
-            self.wiggle_motor.run_angle(
-                speed=-200,
-                rotation_angle=360,
-                then=Stop.HOLD,
-                wait=True)
-
-            self.ev3_brick.speaker.play_file(file=SoundFile.CRYING)
-
-        elif detected_color == Color.GREEN:
-            self.ev3_brick.speaker.play_file(file=SoundFile.GREEN)
-
-            self.wiggle_motor.run_angle(
-                speed=-400,
-                rotation_angle=360,
-                then=Stop.HOLD,
-                wait=True)
-
-            self.ev3_brick.speaker.play_file(file=SoundFile.YES)
-
-            wait(1000)
-
-        elif detected_color == Color.BLUE:
-            self.ev3_brick.speaker.play_file(file=SoundFile.BLUE)
-
-            self.ev3_brick.speaker.play_file(file=SoundFile.FANTASTIC)
-
-            self.ev3_brick.speaker.play_file(file=SoundFile.GOOD_JOB)
-
-            self.wiggle_motor.run_angle(
-                speed=750,
-                rotation_angle=360,
-                then=Stop.HOLD,
-                wait=True)
-
-            self.ev3_brick.speaker.play_file(file=SoundFile.MAGIC_WAND)
+            wait(10)            
 
     def keep_reacting_to_colors(self):
         while True:
-            self.react_to_color()
+            detected_color = self.color_sensor.color()
+
+            if detected_color == Color.YELLOW:
+                self.ev3_brick.speaker.play_file(file=SoundFile.YELLOW)
+
+                self.wiggle_motor.run_angle(
+                    speed=-860,
+                    rotation_angle=360,
+                    then=Stop.HOLD,
+                    wait=True)
+
+                self.ev3_brick.speaker.play_file(file=SoundFile.UH_OH)
+
+                wait(500)
+
+                self.ev3_brick.speaker.play_file(file=SoundFile.SNEEZING)
+
+                wait(500)
+
+            elif detected_color == Color.RED:
+                self.ev3_brick.speaker.play_file(file=SoundFile.SHOUTING)
+
+                for _ in range(randint(1, 6)):
+                    self.ev3_brick.speaker.play_file(file=SoundFile.SMACK)
+
+                self.ev3_brick.light.on(color=Color.RED)
+
+                self.wiggle_motor.run_angle(
+                    speed=170,
+                    rotation_angle=360,
+                    then=Stop.HOLD,
+                    wait=True)
+
+                self.ev3_brick.speaker.play_file(file=SoundFile.LEGO)
+                self.ev3_brick.speaker.play_file(file=SoundFile.MINDSTORMS)
+
+                self.ev3_brick.light.off()
+
+            elif detected_color == Color.BROWN:
+                self.ev3_brick.speaker.play_file(file=SoundFile.BROWN)
+
+                wait(1000)
+
+                self.wiggle_motor.run_angle(
+                    speed=-200,
+                    rotation_angle=360,
+                    then=Stop.HOLD,
+                    wait=True)
+
+                self.ev3_brick.speaker.play_file(file=SoundFile.CRYING)
+
+            elif detected_color == Color.GREEN:
+                self.ev3_brick.speaker.play_file(file=SoundFile.GREEN)
+
+                self.wiggle_motor.run_angle(
+                    speed=-400,
+                    rotation_angle=360,
+                    then=Stop.HOLD,
+                    wait=True)
+
+                self.ev3_brick.speaker.play_file(file=SoundFile.YES)
+
+                wait(1000)
+
+            elif detected_color == Color.BLUE:
+                self.ev3_brick.speaker.play_file(file=SoundFile.BLUE)
+
+                self.ev3_brick.speaker.play_file(file=SoundFile.FANTASTIC)
+
+                self.ev3_brick.speaker.play_file(file=SoundFile.GOOD_JOB)
+
+                self.wiggle_motor.run_angle(
+                    speed=750,
+                    rotation_angle=360,
+                    then=Stop.HOLD,
+                    wait=True)
+
+                self.ev3_brick.speaker.play_file(file=SoundFile.MAGIC_WAND)
+
             wait(10)
