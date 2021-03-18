2311cf89d2bff9a101315a10a7eb00f8231705b8
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2021-03-18 14:30:02 -0700

    fix MVP Basic program

--- sets/inventor/mvp/mvp_basic.py
+++ sets/inventor/mvp/mvp_basic.py
@@ -6,13 +6,14 @@ Robot Inventor App.
 """
 
 from pybricks.pupdevices import Motor
-from pybricks.parameters import Port
+from pybricks.parameters import Direction, Port
 
 
 class MVP:
     def __init__(self):
         self.steer_motor = Motor(Port.A)
-        self.drive_motor = Motor(Port.B)
+        self.drive_motor = Motor(Port.B,
+                                 positive_direction=Direction.COUNTERCLOCKWISE)
 
     def calibrate(self):
         self.steer_motor.run_target(speed=1000, target_angle=0)
@@ -22,6 +23,6 @@ class MVP:
 mvp = MVP()
 mvp.calibrate()
 
-# Make mvp drive in a cirle.
+# Make mvp drive in a circle.
 mvp.steer_motor.run_angle(speed=350, rotation_angle=50)
-mvp.drive_motor.run_angle(speed=800, rotation_angle=-16 * 360)
+mvp.drive_motor.run_angle(speed=800, rotation_angle=16 * 360)
