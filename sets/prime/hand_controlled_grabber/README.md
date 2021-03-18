09801f94bc89524e31c0516d9e4af8c83c7226b3
Ariana Lương Phạm <Ariana.LuongPham@gmail.com>
2021-03-18 14:13:27 -0700

    add Grabber program

---
+++ sets/prime/hand_controlled_grabber/README.md
@@ -0,0 +1,7 @@
+# Example LEGO® SPIKE™ Prime programs for Hand-Controlled Grabbers
+
+These programs require PyBricks v3 firmware installed on the Prime Hub.
+
+Building instructions can be found at https://education.lego.com/en-us/product-resources/spike-prime/downloads/building-instructions.
+
+The playing instructions for each robot variant are in the docstrings of the corresponding file.
---
+++ sets/prime/hand_controlled_grabber/hand_controlled_grabber.py
@@ -0,0 +1,27 @@
+"""
+This program is for Hand-Controlled Grabbers.
+
+Follow the corresponding building instructions in the LEGO® SPIKE™ Prime App.
+
+Press the Force Sensor to grab objects, and release the Force Sensor to let go.
+"""
+
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import ForceSensor, Motor
+from pybricks.parameters import Port
+
+
+# Configure the Hub, the Force Sensor and the Motor
+hub = PrimeHub()
+force_sensor = ForceSensor(Port.E)
+motor = Motor(Port.A)
+
+
+while True:
+    # Grab when the Force Sensor is pressed
+    if force_sensor.pressed():
+        motor.run(speed=-1000)
+
+    # else let go
+    else:
+        motor.run_until_stalled(speed=1000)
