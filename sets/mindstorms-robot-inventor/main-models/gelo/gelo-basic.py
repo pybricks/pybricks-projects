3b45daf41afdd3d6cabf0075f129de5c33442302
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2021-06-08 23:02:06 -0700

    refactor gelo-basic program

--- sets/mindstorms-robot-inventor/main-models/gelo/gelo-basic.py
+++ sets/mindstorms-robot-inventor/main-models/gelo/gelo-basic.py
@@ -1,7 +1,7 @@
 from pybricks.hubs import InventorHub
 from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor
 from pybricks.geometry import Axis
-from pybricks.parameters import Direction, Port, Side, Stop
+from pybricks.parameters import Direction, Port, Side
 from pybricks.tools import wait
 
 
@@ -12,12 +12,10 @@ class Gelo:
         # Configure the leg motors
         self.front_left_leg_motor = Motor(Port.D)
         self.front_right_leg_motor = \
-            Motor(Port.C,
-                  positive_direction=Direction.COUNTERCLOCKWISE)
+            Motor(Port.C, Direction.COUNTERCLOCKWISE)
         self.rear_left_leg_motor = Motor(Port.B)
         self.rear_right_leg_motor = \
-            Motor(Port.A,
-                  positive_direction=Direction.COUNTERCLOCKWISE)
+            Motor(Port.A, Direction.COUNTERCLOCKWISE)
         self.leg_motors = [
             self.front_left_leg_motor, self.front_right_leg_motor,
             self.rear_left_leg_motor, self.rear_right_leg_motor
@@ -31,22 +29,20 @@ class Gelo:
         self.hub.display.orientation(up=Side.TOP)
 
         for _ in range(10):
-            self.hub.display.image(
-                image=[[00, 11, 33, 11, 00],
-                       [11, 33, 33, 33, 11],
-                       [33, 66, 99, 66, 33],
-                       [11, 33, 66, 33, 11],
-                       [00, 11, 33, 11, 00]])
+            self.hub.display.image([[00, 11, 33, 11, 00],
+                                    [11, 33, 33, 33, 11],
+                                    [33, 66, 99, 66, 33],
+                                    [11, 33, 66, 33, 11],
+                                    [00, 11, 33, 11, 00]])
             wait(50)
             self.hub.display.off()
             wait(50)
 
     def straighten_legs(self):
         for leg_motor in self.leg_motors:
-            leg_motor.run_target(
-                speed=1000,
-                target_angle=0,
-                wait=False)
+            leg_motor.run_target(speed=1000,
+                                 target_angle=0,
+                                 wait=False)
 
     def walk(self, speed: int, seconds: int):
         for _ in range(seconds):
